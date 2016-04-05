baa77180348772134cd2086e47f878bf98fec739
Mathieu Delestre <ldbglobe@gmail.com>
2016-04-05 15:35:50 +0200

    Http Cache enhancement

--- Http/Cache.php
+++ Http/Cache.php
@@ -5,6 +5,7 @@
 namespace ldbglobe\tools\Http;
 
 class Cache {
+
 	public static function Init($lastModifiedTimestamp, $maxAge)
 	{
 		if (self::IsModifiedSince($lastModifiedTimestamp))
@@ -41,6 +42,8 @@ class Cache {
 		// Set headers
 		header("HTTP/1.1 304 Not Modified", true);
 		header("Cache-Control: public, max-age=$maxAge", true);
+		header("Expires: ".gmdate('D, d M Y H:i:s', time() + 3600*24*30)." GMT", true);
+		header("Pragma: cache", true);
 		die();
 	}
 
@@ -53,5 +56,17 @@ class Cache {
 		header("HTTP/1.1 200 OK", true);
 		header("Cache-Control: public, max-age=$maxAge", true);
 		header("Last-Modified: $date", true);
+		header("Expires: ".gmdate('D, d M Y H:i:s', time() + 3600*24*30)." GMT", true);
+		header("Pragma: cache", true);
 	}
+
+	public static function Expire($maxAge)
+	{
+		// set a simple expire cache control (no serveur validation)
+		header("HTTP/1.1 200 OK", true);
+		header("Cache-Control: public, max-age=$maxAge", true);
+		header("Expires: ".gmdate('D, d M Y H:i:s', time() + $maxAge)." GMT", true);
+		header("Pragma: cache", true);
+	}
+
 }
