31332ec45158916b4f13dde0253fbe2a4f2ec1d7
Mathieu Delestre <ldbglobe@gmail.com>
2019-10-09 16:05:40 +0200

    fix

--- Http/Cache.php
+++ Http/Cache.php
@@ -4,6 +4,22 @@
 
 namespace ldbglobe\tools\Http;
 
+if (!function_exists('getallheaders'))
+{
+    function getallheaders()
+    {
+           $headers = [];
+       foreach ($_SERVER as $name => $value)
+       {
+           if (substr($name, 0, 5) == 'HTTP_')
+           {
+               $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+           }
+       }
+       return $headers;
+    }
+}
+
 class Cache {
 
 	public static function Init($lastModifiedTimestamp, $maxAge)
