391b5e2e97b8a6cbb07af1ed46a41a696e5b9e7a
Mathieu Delestre <ldbglobe@gmail.com>
2019-10-15 12:33:57 +0200

    fix Entity static parameters handling

--- Entity.php
+++ Entity.php
@@ -10,25 +10,25 @@ class Entity extends \Model {
 
 	static function create($connection_name = NULL) // make a new ORM model instance ready to use and call onCreate hook
 	{
-		return \Model::factory(get_called_class(), $connection_name)->create()->onCreate();
+		return parent::factory(get_called_class(), $connection_name)->create()->onCreate();
 	}
 	static function load($id, $connection_name = NULL) // load an ORM instance from BDD and call onLoad hook
 	{
-		$entity = \Model::factory(get_called_class(), $connection_name)->where('id',$id)->find_one();
+		$entity = parent::factory(get_called_class(), $connection_name)->where('id',$id)->find_one();
 		return $entity ? $entity->onLoad() : false;
 	}
 	static function factory($class_name=null, $connection_name = NULL) // return ORM wrapper instance to make querying through idiorm
 	{
-		return \Model::factory($class_name ? $class_name:get_called_class(), $connection_name);
+		return parent::factory($class_name ? $class_name:get_called_class(), $connection_name);
 	}
 
 	// event hook (to override in your own entity extended classes)
 
 	function onCreate()
 	{
-		if(self::$_id_mode==='UUID')
+		if(static::$_id_mode==='UUID')
 		{
-			$this->id = (string)\Ramsey\Uuid\Uuid::uuid4();
+			$this->{static::$_id_column} = (string)\Ramsey\Uuid\Uuid::uuid4();
 		}
 
 		// override this function in your entity declaration
