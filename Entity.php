7be9345dfcb6996db7809900ab12455192fce535
Mathieu Delestre <ldbglobe@gmail.com>
2019-10-15 10:44:27 +0200

    test for an UUID ORM key handling

--- Entity.php
+++ Entity.php
@@ -3,6 +3,22 @@ namespace ldbglobe\tools;
 
 class Entity extends \Model {
 
+	static private $__settings = array(
+		'id_mode'=>'autoinc', //[autoinc, UUID]
+	);
+
+	static function Settings($k,$v)
+	{
+		if(isset(self::$__settings[$k]))
+		{
+			self::$__settings[$k] = $v;
+		}
+		else
+		{
+			throw new Exception("Entity settings \"$k\" does not exist", 1);
+		}
+	}
+
 	// shortcut to manipulate paris model
 
 	static function create() // make a new ORM model instance ready to use and call onCreate hook
@@ -17,6 +33,17 @@ class Entity extends \Model {
 	static function factory($class_name=null, $connection_name = NULL) // return ORM wrapper instance to make querying through idiorm
 	{
 		return \Model::factory($class_name ? $class_name:get_called_class(), $connection_name);
+	}
+
+	function save()
+	{
+		if(self::$__settings['id_mode']==='UUID')
+		{
+			if(!isset($this->id) || empty($this->id))
+			{
+				$this->id = Ramsey\Uuid\Uuid::uuid4();
+			}
+		}
 	}
 
 	// event hook (to override in your own entity extended classes)
